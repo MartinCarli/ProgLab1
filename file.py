d17bd9cab26d6ff54dc5ecd41244259d0ce831d5
repl.it user <replituser@example.com>
2021-01-04 17:02:16 +0000

    Inizio

---
+++ file.py
@@ -0,0 +1,14 @@
+values = []
+
+my_file = open("shampoo_sales.csv","r")
+for line in my_file:
+    elements = line.split(',')
+ 
+    if elements[0] != 'Date':
+ 
+        date = elements[0]
+        value = elements[1]
+ 
+        values.append(float(value))
+
+print(values)
