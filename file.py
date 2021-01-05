0cc1359a8edf31794a5b5c61f25a4fec030d6b0c
repl.it user <replituser@example.com>
2021-01-05 10:41:43 +0000

    funziona?

--- file.py
+++ file.py
@@ -1,14 +1,24 @@
-values = []
+def list_sum(the_list):
+    total=0
+    for item in the_list:
+        total = total + item
+    return total
 
-my_file = open("shampoo_sales.csv","r")
+
+# Inizializzo una lista vuota per salvare i valori
+values = []
+# Apro e leggo il file, linea per linea
+my_file = open("shampoo_sales.txt", "r")
 for line in my_file:
+ # Faccio lo split di ogni riga sulla virgola
     elements = line.split(',')
- 
+ # Se NON sto processando l’intestazione...
     if elements[0] != 'Date':
- 
+
         date = elements[0]
         value = elements[1]
- 
-        values.append(float(value))
+    values.append(float(value))
 
-print(values)
+
+x= list_sum(values)
+print(x)
